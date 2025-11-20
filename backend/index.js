2575bc5f52c927f36575c9f1288d30fa4fdc0c34
Nihar-tech-21 <niharika.dhaka.20@gmail.com>
2025-11-20 17:24:11 +0530

    Initial commit with full project structure, ignoring .env files

---
+++ backend/index.js
@@ -0,0 +1,158 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { createSecretToken } = require("./utils/secretToken");
+
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
+const UsersModel = require("./model/UsersModel");
+
+const PORT = process.env.PORT;
+const DBurl = process.env.MONGO_URL;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL], //"*" for all origins
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+  newOrder.save();
+  res.send("Order saved!");
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await UsersModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new UsersModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 2,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed in successfully", success: true, user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UsersModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 1000 * 60 * 60 * 1,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true, user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/me", async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No token found", success: false });
+    }
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const user = await UsersModel.findById(decoded.id).select("-password"); // exclude password
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(401)
+      .json({ message: "Invalid or expired token", success: false });
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: false, // true in production if using HTTPS
+    sameSite: "lax", // must match your cookie setup
+    path: "/",
+  });
+
+  res.status(200).json({ message: "Logged out successfully", success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${process.env.PORT}`);
+  mongoose.connect(DBurl);
+  console.log("DB connected");
+});
